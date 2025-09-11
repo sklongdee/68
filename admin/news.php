b606d5d26501f75e2c25f6e76bf845aff625d3d2
SUTTIPONG KLONGDEE <stp.suttipong@gmail.com>
2025-09-11 14:09:39 +0700

    added all files

---
+++ admin/news.php
@@ -0,0 +1,6 @@
+<div class="container-fluid px-4">
+    <h1 class="mt-4">จัดการข่าวสาร</h1>
+        <ol class="breadcrumb mb-4">
+            <li class="breadcrumb-item active">จัดการข่าวสาร</li>
+        </ol>
+</div>
---
+++ news.php
@@ -0,0 +1,72 @@
+    <!-- ข่าวสาร -->
+    <div class="container py-3">
+        <h2>ข่าวกิจกรรม </h2>
+        <div class="row row-cols-1 row-cols-md-3 g-4">
+            <div class="col">
+                <div class="card h-100">
+                <img src="news/21.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">โครงการนักศึกษาร่วมใจอาสาพัฒนามหาวิทยาลัย</h5>
+                </div>
+                <div class="card-footer">
+                    <small class="text-body-secondary">Last updated 3 mins ago</small>
+                </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100">
+                <img src="news/22.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">(19 ส.ค.68) มุมมองเศรษฐกิจจันทบุรี 2568 วิเคราะห์สถานการณ์ สู่การวางนโยบายเชิงพื้นที่</h5>
+                </div>
+                <div class="card-footer">
+                    <small class="text-body-secondary">Last updated 3 mins ago</small>
+                </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100">
+                <img src="news/23.jpeg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">(15 ส.ค.68) ลงนามบันทึกความเข้าใจความร่วมมือทางวิชาการ (MOU) กับ วิทยาลัยเทคนิคตราด </h5>
+                </div>
+                <div class="card-footer">
+                    <small class="text-body-secondary">Last updated 3 mins ago</small>
+                </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100">
+                <img src="news/24.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">( 14 ส.ค.68) วันทาคณาจารย์และมอบตัวเป็นศิษย์ ประจำปีการศึกษา 2568</h5>
+                </div>
+                <div class="card-footer">
+                    <small class="text-body-secondary">Last updated 3 mins ago</small>
+                </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100">
+                <img src="news/24.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">(12 ส.ค.68) วางพานพุ่มเงิน พุ่มทอง สมเด็จพระนางเจ้าสิริกิติ์ พระบรมราชินีนาถ พระบรมราชชนนีพันปีหลวง</h5>
+                </div>
+                <div class="card-footer">
+                    <small class="text-body-secondary">Last updated 3 mins ago</small>
+                </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100">
+                <img src="news/26.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">(12 ส.ค.68) เจริญพระพุทธมนต์ เนื่องในโอกาสวันเฉลิมพระชนมพรรษา สมเด็จพระนางเจ้าสิริกิติ์ พระบรมราชินี</h5>
+                </div>
+                <div class="card-footer">
+                    <small class="text-body-secondary">Last updated 3 mins ago</small>
+                </div>
+                </div>
+            </div>
+        </div>
+    </div>
