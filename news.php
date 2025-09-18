90e6f63c7a59555d2ba62d7cd4d46593edbd613f
SUTTIPONG KLONGDEE <stp.suttipong@gmail.com>
2025-09-18 15:09:41 +0700

    first commit

--- news.php
+++ news.php
@@ -1,7 +1,17 @@
     <!-- ข่าวสาร -->
     <div class="container py-3">
-        <h2>ข่าวกิจกรรม </h2>
-        <div class="row row-cols-1 row-cols-md-3 g-4">
+        <?php
+            include_once "admin/conn.php";
+            $sql_type = "SELECT * FROM news_type";
+            $stmt_type = $conn->prepare($sql_type);
+            $stmt_type->execute();
+            $result_type = $stmt_type->fetchAll(PDO::FETCH_ASSOC);
+            foreach ($result_type as $row_type) {
+                $news_type_id=$row_type['news_type_id'];
+        ?>
+
+        <h2><?=$row_type['news_type_name']?></h2>
+        <div class="row row-cols-1 row-cols-md-3 g-4 mb-5">
             <div class="col">
                 <div class="card h-100">
                 <img src="news/21.jpg" class="card-img-top" alt="...">
@@ -13,61 +23,11 @@
                 </div>
                 </div>
             </div>
-            <div class="col">
-                <div class="card h-100">
-                <img src="news/22.jpg" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">(19 ส.ค.68) มุมมองเศรษฐกิจจันทบุรี 2568 วิเคราะห์สถานการณ์ สู่การวางนโยบายเชิงพื้นที่</h5>
-                </div>
-                <div class="card-footer">
-                    <small class="text-body-secondary">Last updated 3 mins ago</small>
-                </div>
-                </div>
-            </div>
-            <div class="col">
-                <div class="card h-100">
-                <img src="news/23.jpeg" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">(15 ส.ค.68) ลงนามบันทึกความเข้าใจความร่วมมือทางวิชาการ (MOU) กับ วิทยาลัยเทคนิคตราด </h5>
-                </div>
-                <div class="card-footer">
-                    <small class="text-body-secondary">Last updated 3 mins ago</small>
-                </div>
-                </div>
-            </div>
-            <div class="col">
-                <div class="card h-100">
-                <img src="news/24.jpg" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">( 14 ส.ค.68) วันทาคณาจารย์และมอบตัวเป็นศิษย์ ประจำปีการศึกษา 2568</h5>
-                </div>
-                <div class="card-footer">
-                    <small class="text-body-secondary">Last updated 3 mins ago</small>
-                </div>
-                </div>
-            </div>
-            <div class="col">
-                <div class="card h-100">
-                <img src="news/24.jpg" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">(12 ส.ค.68) วางพานพุ่มเงิน พุ่มทอง สมเด็จพระนางเจ้าสิริกิติ์ พระบรมราชินีนาถ พระบรมราชชนนีพันปีหลวง</h5>
-                </div>
-                <div class="card-footer">
-                    <small class="text-body-secondary">Last updated 3 mins ago</small>
-                </div>
-                </div>
-            </div>
-            <div class="col">
-                <div class="card h-100">
-                <img src="news/26.jpg" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">(12 ส.ค.68) เจริญพระพุทธมนต์ เนื่องในโอกาสวันเฉลิมพระชนมพรรษา สมเด็จพระนางเจ้าสิริกิติ์ พระบรมราชินี</h5>
-                </div>
-                <div class="card-footer">
-                    <small class="text-body-secondary">Last updated 3 mins ago</small>
-                </div>
-                </div>
-            </div>
         </div>
+        <?php
+            }
+        ?>
+
+
     </div>
  
