04828bbc111c9105433de8a931d86acd1978e3cc
SUTTIPONG KLONGDEE <stp.suttipong@gmail.com>
2025-09-25 13:32:00 +0700

    first commit

--- index.php
+++ index.php
@@ -58,10 +58,7 @@
                 <a class="nav-link disabled" aria-disabled="true">Disabled</a>
                 </li>
             </ul>
-            <form class="d-flex" role="search">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-                <button class="btn btn-outline-success" type="submit">Search</button>
-            </form>
+                <a href="admin" class="btn btn-outline-success" type="submit">Login</a>
             </div>
         </div>
     </nav>
