37c82941086424a724f0e577b464889cf3c02d62
SUTTIPONG KLONGDEE <stp.suttipong@gmail.com>
2025-10-02 16:14:33 +0700

    first commit

--- admin/update_news_verify.php
+++ admin/update_news_verify.php
@@ -1,11 +1,13 @@
 <?php
 if (isset($_FILES['news_img']) && $_FILES['news_img']['error'] === UPLOAD_ERR_OK) {
+    echo "มีไฟล์อัพโหลดมา<br>";
     echo $_POST["news_id"];
     echo $_POST["news_title"];
     echo $_POST["news_detail"];
     echo $_FILES["news_img"]["name"];
     echo $_POST["news_type"];
 } else {
+    echo "ไม่มีไฟล์อัพโหลดมา<br>";
     echo $_POST["news_id"];
     echo $_POST["news_title"];
     echo $_POST["news_detail"];
